21b99a0c32b18c1c4553c92bf9b457d6317f1d54
Cuprumer <62464923+k89-ux@users.noreply.github.com>
2020-08-23 22:55:08 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 import { Project } from "https://unpkg.com/leopard@^1/dist/index.esm.js";
 
 import Stage from "./Stage/Stage.js";
+import Watermelon from "./Watermelon/Watermelon.js";
 
 const stage = new Stage({ costumeNumber: 1 });
 
-const sprites = {};
+const sprites = {
+  Watermelon: new Watermelon({
+    x: 0,
+    y: 0,
+    direction: 90,
+    costumeNumber: 2,
+    size: 200,
+    visible: true,
+    layerOrder: 1
+  })
+};
 
 const project = new Project(stage, sprites, {
   frameRate: 30 // Set to 60 to make your project run faster
